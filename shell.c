c2c3f824bbd87ae79e980e9ab78b6ed836587d69
Ulrik Damm <ulrikdamm@me.com>
2012-09-20 11:25:01 +0200

    Piping works but hangs

--- shell.c
+++ shell.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+void shell_run_command_with_pipe(shell *s, cmd *c, int read_pipe);
+
 shell *shell_alloc() {
 	shell *s = salloc(sizeof(shell));
 	s->path = "~/";
@@ -22,9 +24,18 @@ void shell_print_prompt(shell *s) {
 }
 
 void shell_run_command(shell *s, cmd *c) {
-	s = NULL;
+	shell_run_command_with_pipe(s, c, 0);
+}
+
+void shell_run_command_with_pipe(shell *s, cmd *c, int read_pipe) {
+	int fds[2];
+	int proc_pid;
 	
-	if (!fork()) {
+	if (c->pipe_to != NULL) {
+		pipe(fds);
+	}
+	
+	if (!(proc_pid = fork())) {
 		cmd_arg *args_p = c->command;
 		int args_count = 0;
 		while (args_p != NULL) {
@@ -43,12 +54,18 @@ void shell_run_command(shell *s, cmd *c) {
 		}
 		args[i] = NULL;
 		
-		if (c->in) {
+		if (read_pipe > 0) {
+			close(fileno(stdin));
+			dup(read_pipe);
+		} else if (c->in) {
 			close(fileno(stdin));
 			dup(fileno(fopen(c->in, "r")));
 		}
 		
-		if (c->out) {
+		if (c->pipe_to) {
+			close(fileno(stdout));
+			dup(fds[1]);
+		} else if (c->out) {
 			close(fileno(stdout));
 			dup(fileno(fopen(c->out, (c->out_append? "a": "w"))));
 		}
@@ -64,5 +81,15 @@ void shell_run_command(shell *s, cmd *c) {
 		free(args);
 	}
 	
-	wait(NULL);
+	if (c->pipe_to != NULL) {
+		shell_run_command_with_pipe(s, c->pipe_to, fds[0]);
+		
+		close(fds[0]);
+		close(fds[1]);
+	}
+	
+	int exit_code;
+	waitpid(proc_pid, &exit_code, 0);
+	
+	printf("Process %s ended with exit code %i\n", c->command->string, exit_code);
 }
